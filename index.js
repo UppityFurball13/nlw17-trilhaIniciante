706ad5aef4d78ef9b90acc58f568ac3a3fcc11fd
JYuriAnderson <jyuri.2014@outlook.com>
2024-09-19 12:42:44 -0300

    add register mark

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const { select } = require('@inquirer/prompts');
+const { select, input } = require('@inquirer/prompts');
+
+let meta = {
+  value: "Tomar 3L de água por dia",
+  checked: false
+}
+
+let metas = [meta]
+
+const cadastrarMeta = async () => {
+  const meta = await input({ message: "Digite a meta:" })
+  if (meta.lenght == 0) {
+    console.log("A meta não pode ser vazia.")
+    return
+  }
+  metas.push({ value: meta, checked: false })
+}
 const start = async () => {
   while (true) {
     const opcao = await select({
@@ -20,10 +36,11 @@ const start = async () => {
     })
     switch (opcao) {
       case "cadastrar":
-        console.log("Vamos cadastrar")
+        await cadastrarMeta()
+        console.log(metas)
         break
       case "listar":
-        console.log("Vamos listar")
+        console.log("listarMetas")
         break
       case "sair":
         console.log("Saindo...")
