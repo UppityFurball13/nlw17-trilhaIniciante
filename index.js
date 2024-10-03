2a61c115d5cab975c79b6fb6038123eb8a42b08c
João Yuri Anderson <jyuri.2014@outlook.com>
2024-10-03 18:33:04 -0300

    adding persist data

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const { select, input, checkbox } = require('@inquirer/prompts');
 
+const fs = require('fs').promises
+
 let mensagem = "Bem-vindo ao app de metas";
 
-let meta = {
-  value: "Tomar 3L de água por dia",
-  checked: false
+const carregarMetas = async () => {
+  try{
+    const dados = await fs.readFile("metas.json", "utf8")
+    metas = JSON.parse(dados)
+  }
+  catch(erro) {
+    metas = []
+  }
 }
 
-let metas = [meta]
+const salvarMetas = async () => {
+  await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
+}
 
 const cadastrarMeta = async () => {
   const meta = await input({ message: "Digite a meta:" })
@@ -21,6 +30,10 @@ const cadastrarMeta = async () => {
 }
 
 const listarMeta = async () => {
+  if (metas.length == 0) {
+    mensagem = "Não existem metas!"
+    return
+  }
   const respostas = await checkbox({
     message: "Use as setas para mudar de metas, o espaço para marcar ou desmarcar e o enter para finalizar essa etapa.",
     choices: [...metas],
@@ -44,6 +57,10 @@ const listarMeta = async () => {
 }
 
 const metasRealizadas = async () => {
+  if (metas.length == 0) {
+    mensagem = "Não existem metas!"
+    return
+  }
   const realizadas = metas.filter((meta) => {
     return meta.checked
   })
@@ -58,6 +75,10 @@ const metasRealizadas = async () => {
 }
 
 const metasAbertas = async () => {
+  if (metas.length == 0) {
+    mensagem = "Não existem metas!"
+    return
+  }
   const abertas = metas.filter((meta) => {
     return meta.checked != true
   })
@@ -72,6 +93,10 @@ const metasAbertas = async () => {
 }
 
 const deletarMetas = async () => {
+  if (metas.length == 0) {
+    mensagem = "Não existem metas!"
+    return
+  }
   const metasDesmarcadas = metas.map((meta) => {
     return {value: meta.value, checked: false}
   })
@@ -103,8 +128,10 @@ const mostrarMensagem = () => {
 }
 
 const start = async () => {
+  await carregarMetas()
   while (true) {
     mostrarMensagem()
+    await salvarMetas()
     const opcao = await select({
       message: "Menu >",
       choices: [
